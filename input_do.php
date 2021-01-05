afccb144dcd0023a1fc8b6b6f679f961953ce02d
shofujimoto0617 <shofujimoto017@gmail.com>
2021-01-05 19:37:19 +0900

    add require

--- input_do.php
+++ input_do.php
@@ -19,16 +19,12 @@
 <h2>Practice</h2>
 <pre>
     <?php
-    try {
-        $db = new PDO('mysql:dbname=mydb;host=localhost;charset=utf8', 'root', 'root');
+    require('dbconnect.php');
 
-        $statement = $db->prepare('INSERT INTO memos SET memo=?, created_at=NOW()');
-        $statement->execute(array($_POST['memo']));
-        echo 'メッセージが登録されました';
-    } catch(PDOException $e) {
-        echo 'DB接続エラー: ' . $e->getMessage();
-    }
-    ?>
+    $statement = $db->prepare('INSERT INTO memos SET memo=?, created_at=NOW()');
+    $statement->execute(array($_POST['memo']));
+    echo 'メッセージが登録されました';
+?>
 </pre>
 </main>
 </body>    
