36e0e0a3b1b6cf72d2ca42574bcaf5e16e9fefb5
shofujimoto0617 <shofujimoto017@gmail.com>
2021-01-05 14:07:43 +0900

    add show

--- memo.php
+++ memo.php
@@ -24,16 +24,20 @@ try {
     echo 'DB接続エラー:' . $e->getMessage();
 }
 
-$memos = $db->query('SELECT * FROM memos ORDER BY id DESC');
+$id = $_REQUEST['id'];
+if (!is_numeric($id) || $id <=0) {
+    print('1以上の数字で指定してください');
+    exit();
+}
 
+$memos = $db->prepare('SELECT * FROM memos WHERE id=?');
+$memos->execute(array($id));
+$memo = $memos->fetch();
 ?>
-
 <article>
-    <?php while ($memo = $memos->fetch()): ?>
-        <p><a href="memo.php?id=<?php print($memo['id']); ?>"><?php print(mb_substr($memo['memo'], 0, 50)); ?></a></p>
-        <time><?php print($memo['created_at']); ?></time>
-        <hr>
-    <?php endwhile; ?>
+    <pre><?php print($memo['memo']); ?></pre>
+
+    <a href="index.php">戻る</a>
 </article>
 
 </main>
