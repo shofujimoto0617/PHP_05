7d5c5675a60a7a747724127bdd0457afd43e43c9
shofujimoto0617 <shofujimoto017@gmail.com>
2021-01-05 13:35:10 +0900

    add form

---
+++ input_do.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="ja">
+<head>
+<!-- Required meta tags -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="css/style.css">
+
+<title>PHP</title>
+</head>
+<body>
+<header>
+<h1 class="font-weight-normal">PHP</h1>    
+</header>
+
+<main>
+<h2>Practice</h2>
+<pre>
+    <?php
+    try {
+        $db = new PDO('mysql:dbname=mydb;host=localhost;charset=utf8', 'root', 'root');
+
+        $statement = $db->prepare('INSERT INTO memos SET memo=?, created_at=NOW()');
+        $statement->execute(array($_POST['memo']));
+        echo 'メッセージが登録されました';
+    } catch(PDOException $e) {
+        echo 'DB接続エラー: ' . $e->getMessage();
+    }
+    ?>
+</pre>
+</main>
+</body>    
+</html>
